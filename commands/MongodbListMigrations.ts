6580db92bfa7a4c752cf39c2c084ad2d8b67b500
Daniel Kostro <kostro.d@gmail.com>
2020-09-09 09:58:33 +0200

    fix: show accurate information in status command

--- commands/MongodbListMigrations.ts
+++ commands/MongodbListMigrations.ts
@@ -1,4 +1,7 @@
+import { inject } from '@adonisjs/fold';
 import CliTable from 'cli-table3';
+
+import { MongodbContract } from '@ioc:Mongodb/Database';
 
 import MigrationCommand from './util/MigrationCommand';
 
@@ -9,26 +12,55 @@ export default class MongodbListMigrations extends MigrationCommand {
     loadApp: true,
   };
 
-  public async handle(): Promise<void> {
-    const migrationNames = await this.getMigrationFiles();
+  @inject(['Mongodb/Database'])
+  public async handle(db: MongodbContract): Promise<void> {
+    if (this.connection && !db.hasConnection(this.connection)) {
+      this.logger.error(
+        `No MongoDB connection registered with name "${this.connection}"`,
+      );
+      process.exitCode = 1;
+      return;
+    }
+    try {
+      const database = await db.connection().database();
+      const coll = database.collection('__adonis_mongodb');
+      const migrationNames = await this.getMigrationFiles();
 
-    const table = new CliTable({
-      head: ['Name', 'Status', 'Batch', 'Message'],
-    });
+      const migrationDocuments = await coll.find({}).toArray();
 
-    /**
-     * Push a new row to the table
-     */
-    migrationNames.forEach((migrationName) => {
-      table.push([
-        migrationName,
-        this.colors.yellow('pending'),
-        'NA',
-        '',
-      ] as any);
-    });
+      const table = new CliTable({
+        head: ['Name', 'Status', 'Batch', 'Message'],
+      });
 
-    // eslint-disable-next-line no-console
-    console.log(table.toString());
+      const imports = await Promise.all(
+        migrationNames.map((migrationName) =>
+          this.importMigration(migrationName),
+        ),
+      );
+
+      /**
+       * Push a new row to the table
+       */
+      migrationNames.forEach((migrationName, idx) => {
+        const document = migrationDocuments.find(
+          (doc) => doc.name === migrationName,
+        );
+
+        const { description } = imports[idx];
+        table.push([
+          migrationName,
+          document
+            ? this.colors.green('completed')
+            : this.colors.yellow('pending'),
+          document ? document.batch : 'NA',
+          description || '',
+        ] as any);
+      });
+
+      // eslint-disable-next-line no-console
+      console.log(table.toString());
+    } finally {
+      void db.closeConnections();
+    }
   }
 }
