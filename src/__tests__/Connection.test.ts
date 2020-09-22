3cd539023d12f0920f53c67c91c1a4c7f8a80836
Xavier Stouder <xavier@stouder.io>
2020-09-22 09:47:09 +0200

    test: setup tests (#9)

---
+++ src/__tests__/Connection.test.ts
@@ -0,0 +1,41 @@
+import { promisify } from 'util';
+
+import { FakeLogger } from '@adonisjs/logger/build/standalone';
+
+import { Connection } from '../Connection';
+
+const sleep = promisify(setTimeout);
+
+const loggerConfig = {
+  name: 'adonis-logger',
+  level: 'trace',
+  messageKey: 'msg',
+  enabled: true,
+};
+
+const connectionConfig = {
+  url: 'mongodb://localhost:33333',
+  database: 'test-runner',
+};
+const logger = new FakeLogger(loggerConfig);
+const connection = new Connection('mongo', connectionConfig, logger);
+
+afterAll(async () => {
+  await connection.close();
+});
+
+test('try to connect with good config', async () => {
+  connection.connect();
+  await sleep(500);
+  expect(logger.logs[logger.logs.length - 1]).toBeUndefined();
+});
+
+test('get collection', async () => {
+  const collection = await connection.collection('test');
+  expect(collection).toBeDefined();
+});
+
+test('get database', async () => {
+  const database = await connection.database();
+  expect(database).toBeDefined();
+});
