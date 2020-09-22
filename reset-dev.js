bed09e2a002dcae36eb546e84b8607396742b885
Michaël Zasso <targos@protonmail.com>
2020-09-22 18:09:51 +0200

    fix?

--- reset-dev.js
+++ reset-dev.js
@@ -15,10 +15,12 @@ try {
       'rs.initiate({ _id: "rs0", members: [{ _id: 0, host: "127.0.0.1:27017" }] })',
     ],
     {
+      // Set COMPOSE_INTERACTIVE_NO_CLI, otherwise Compose tries to allocate a TTY when it cannot in GitHub actions.
+      env: { ...process.env, COMPOSE_INTERACTIVE_NO_CLI: 'true' },
       stdio: 'inherit',
     },
   );
 } catch (e) {
-  process.stderr.write(e.stderr)
-  process.exit(1)
+  process.stderr.write(e.stderr);
+  process.exit(1);
 }
