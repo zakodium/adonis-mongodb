4dba0ef9cef484fd9468519ccdb0b6f8f2219b0f
Michaël Zasso <targos@protonmail.com>
2020-11-17 20:45:54 +0100

    chore: fix test configuration

--- reset-dev.js
+++ reset-dev.js
@@ -13,8 +13,9 @@ try {
       '-T',
       'mongodb',
       'mongo',
+      '127.0.0.1:33333',
       '--eval',
-      'rs.initiate({ _id: "rs0", members: [{ _id: 0, host: "127.0.0.1:27017" }] })',
+      'rs.initiate({ _id: "rs0", members: [{ _id: 0, host: "127.0.0.1:33333" }] })',
     ],
     {
       stdio: 'inherit',
