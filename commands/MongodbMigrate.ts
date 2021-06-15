0b4fb928798e19c831f86ee1d26d59d5473fac75
Xavier Stouder <xavier@stouder.io>
2021-06-15 14:52:59 +0200

    feat: do not cancel successful migrations (#65)

--- commands/MongodbMigrate.ts
+++ commands/MongodbMigrate.ts
@@ -28,64 +28,66 @@ export default class MongodbMigrate extends MigrationCommand {
     const connectionName = this.connection || undefined;
     const connection = db.connection(connectionName);
 
-    await connection.transaction(async (session) => {
-      const migrationLockColl = await connection.collection(
-        migrationLockCollectionName,
-      );
+    const migrationLockColl = await connection.collection(
+      migrationLockCollectionName,
+    );
 
-      const migrationColl = await connection.collection<IMigration>(
-        migrationCollectionName,
-      );
+    const migrationColl = await connection.collection<IMigration>(
+      migrationCollectionName,
+    );
 
-      const lock = await migrationLockColl.updateOne(
+    const lock = await migrationLockColl.updateOne(
+      {
+        _id: 'migration_lock',
+      },
+      {
+        $set: { running: true },
+      },
+      {
+        upsert: true,
+      },
+    );
+
+    if (lock.modifiedCount === 0 && lock.upsertedCount === 0) {
+      this.logger.error('A migration is already running');
+      process.exitCode = 1;
+      await db.closeConnections();
+      return;
+    }
+
+    let migrationDocsCursor = migrationColl.find({});
+    const migrationDocs = await migrationDocsCursor.toArray();
+    const dbMigrationNames = migrationDocs.map((m) => m.name);
+
+    // Keep migrations that are not yet registered
+    const unregisteredMigrations = migrations.filter(
+      (migration) => !dbMigrationNames.includes(migration.name),
+    );
+
+    // Keep migrations that are not yet registered
+    let successfullyExecuted = 0;
+
+    // Get the next incremental batch value
+    const value = await migrationColl
+      .aggregate<{ maxBatch: number }>([
         {
-          _id: 'migration_lock',
+          $project: {
+            maxBatch: { $max: '$batch' },
+          },
         },
-        {
-          $set: { running: true },
-        },
-        {
-          upsert: true,
-        },
-      );
+      ])
+      .toArray();
 
-      if (lock.modifiedCount === 0 && lock.upsertedCount === 0) {
-        this.logger.error('A migration is already running');
-        process.exitCode = 1;
-        await db.closeConnections();
-        return;
-      }
+    let newBatch = 1;
+    if (value.length === 1) {
+      newBatch = value[0].maxBatch + 1;
+    }
 
-      try {
-        let migrationDocsCursor = migrationColl.find({});
-        const migrationDocs = await migrationDocsCursor.toArray();
-        const dbMigrationNames = migrationDocs.map((m) => m.name);
-
-        // Keep migrations that are not yet registered
-        const unregisteredMigrations = migrations.filter(
-          (migration) => !dbMigrationNames.includes(migration.name),
-        );
-
-        // Keep migrations that are not yet registered
-        let executed = 0;
-
-        // Get the next incremental batch value
-        const value = await migrationColl
-          .aggregate<{ maxBatch: number }>([
-            {
-              $project: {
-                maxBatch: { $max: '$batch' },
-              },
-            },
-          ])
-          .toArray();
-
-        let newBatch = 1;
-        if (value.length === 1) {
-          newBatch = value[0].maxBatch + 1;
-        }
-
-        for (const { name, file } of unregisteredMigrations) {
+    let lastTransactionError = null;
+    for (const { name, file } of unregisteredMigrations) {
+      // eslint-disable-next-line @typescript-eslint/no-loop-func
+      await connection.transaction(async (session) => {
+        try {
           const { Migration, description } = await this.importMigration(file);
 
           this.logger.info(
@@ -95,47 +97,62 @@ export default class MongodbMigrate extends MigrationCommand {
           );
           const migration = new Migration(connectionName, this.logger, session);
           await migration.execUp();
-          executed++;
-        }
 
-        if (unregisteredMigrations.length > 0) {
-          await migrationColl.insertMany(
-            unregisteredMigrations.map(
-              ({ name }) => ({
-                name,
-                date: new Date(),
-                batch: newBatch,
-              }),
-              {
-                session,
-              },
-            ),
+          await migrationColl.insertOne(
+            {
+              name,
+              date: new Date(),
+              batch: newBatch,
+            },
+            { session },
           );
+        } catch (err) {
+          lastTransactionError = err;
+          await session.abortTransaction();
         }
+      });
 
-        if (executed > 0) {
-          this.logger.info(`Executed ${executed} migrations`);
-        } else {
-          this.logger.info('No pending migration');
-        }
-      } catch (err) {
-        this.logger.error('Migration failed');
-        // TODO: See if there can be a way in Ace commands to print error stack traces
-        // eslint-disable-next-line no-console
-        console.error(err);
-        await session.abortTransaction();
-      } finally {
-        await migrationLockColl.updateOne(
-          {
-            _id: 'migration_lock',
-            running: true,
-          },
-          {
-            $set: { running: false },
-          },
-        );
+      if (lastTransactionError) {
+        break;
       }
-    });
+
+      successfullyExecuted++;
+    }
+
+    await migrationLockColl.updateOne(
+      {
+        _id: 'migration_lock',
+        running: true,
+      },
+      {
+        $set: { running: false },
+      },
+    );
+
+    if (successfullyExecuted > 0) {
+      const remainingMigrations =
+        unregisteredMigrations.length - successfullyExecuted;
+      this.logger.info(
+        `Successfully executed ${successfullyExecuted} migrations${
+          lastTransactionError ? `, 1 migration failed` : ''
+        }${
+          remainingMigrations > 0
+            ? `, ${
+                remainingMigrations - (lastTransactionError ? 1 : 0)
+              } pending migrations remaining`
+            : ''
+        }`,
+      );
+    } else if (lastTransactionError === null) {
+      this.logger.info('No pending migration');
+    }
+
+    if (lastTransactionError) {
+      this.logger.error('Migration failed');
+      // TODO: See if there can be a way in Ace commands to print error stack traces
+      // eslint-disable-next-line no-console
+      console.error(lastTransactionError);
+    }
   }
 
   @inject(['Mongodb/Database'])
