093eef34799544aedc929837690409fbdbe38582
Michaël Zasso <targos@protonmail.com>
2021-07-14 14:08:36 +0200

    refactor!: move ObjectId export to Zakodium/Mongodb/Odm

--- adonis-typings/container.ts
+++ adonis-typings/container.ts
@@ -1,13 +1,11 @@
 declare module '@ioc:Adonis/Core/Application' {
   import { DatabaseContract } from '@ioc:Zakodium/Mongodb/Database';
   import Migration from '@ioc:Zakodium/Mongodb/Migration';
-  import ObjectId from '@ioc:Zakodium/Mongodb/ObjectId';
   import * as Odm from '@ioc:Zakodium/Mongodb/Odm';
 
   export interface ContainerBindings {
     'Zakodium/Mongodb/Database': DatabaseContract;
     'Zakodium/Mongodb/Odm': typeof Odm;
     'Zakodium/Mongodb/Migration': typeof Migration;
-    'Zakodium/Mongodb/ObjectId': typeof ObjectId;
   }
 }
