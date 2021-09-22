37ff44ed071290e05c59b85fa20667ba4b472dac
Michaël Zasso <targos@protonmail.com>
2021-09-22 11:45:19 +0200

    fix: throw an error when user attempts to spread a model
    Refs: https://github.com/zakodium/adonis-mongodb/issues/114

--- src/Model/proxyHandler.ts
+++ src/Model/proxyHandler.ts
@@ -1,16 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export const proxyHandler = {
-  get(target: any, prop: string) {
+export const proxyHandler: ProxyHandler<any> = {
+  get(target: any, prop: string | symbol) {
     if (typeof target[prop] !== 'undefined') {
       return Reflect.get(target, prop);
     }
-    return Reflect.get(target.$currentData, prop);
+    return Reflect.get(target.$attributes, prop);
   },
-  set(target: any, prop: string, value: any) {
+  set(target: any, prop: string | symbol, value: any) {
     if (typeof target[prop] !== 'undefined') {
       return Reflect.set(target, prop, value);
     }
-    return Reflect.set(target.$currentData, prop, value);
+    return Reflect.set(target.$attributes, prop, value);
+  },
+  ownKeys() {
+    throw new Error(
+      'Getting model keys is disallowed. If you want to use object spread on the current data, do { ...model.$attributes }',
+    );
   },
 };
