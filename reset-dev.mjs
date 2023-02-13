3bda8df5d2d0804e654751b7ba63452b287a7a29
Michaël Zasso <targos@protonmail.com>
2023-02-13 14:38:20 +0100

    fixup

--- reset-dev.mjs
+++ reset-dev.mjs
@@ -14,7 +14,7 @@ try {
       'mongosh',
       '127.0.0.1:33333',
       '--eval',
-      'db.disableFreeMonitoring(); rs.initiate({ _id: "rs0", members: [{ _id: 0, host: "127.0.0.1:33333" }] });',
+      'rs.initiate({ _id: "rs0", members: [{ _id: 0, host: "127.0.0.1:33333" }] });',
     ],
     {
       stdio: 'inherit',
